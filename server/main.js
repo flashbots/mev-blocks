f4d1df78f4e4146204fede9ae71236588d8f23b8
Jason Paryani <github@jparyani.com>
2021-04-02 09:53:25 -0700

    Fix examples for new string fields

--- server/main.js
+++ server/main.js
@@ -51,9 +51,9 @@ const sql = postgres(process.env.POSTGRES_DSN)
       "eoa_address": "0x421125ca608A35458B2C99DA39CD55B70bA202a4",
       "to_address": "0xa57Bd00134B2850B2a1c55860c9e9ea100fDd6CF",
       "gas_used": 79188,
-      "gas_price": 0,
-      "coinbase_transfer": 10340046243502720,
-      "total_miner_reward": 10340046243502720
+      "gas_price": "0",
+      "coinbase_transfer": "10340046243502720",
+      "total_miner_reward": "10340046243502720"
     },
     {
       "transaction_hash": "0x965aa095d75f03ba91851ce3b8f1b51fee09ae0de837e42652412b6ace18691f",
@@ -62,9 +62,9 @@ const sql = postgres(process.env.POSTGRES_DSN)
       "eoa_address": "0x1F00ACFEdC298253487D91758bcfe9D7a6Ba2c83",
       "to_address": "0xb10E56Edb7698C960f1562c7edcC15612900c4A5",
       "gas_used": 103577,
-      "gas_price": 0,
-      "coinbase_transfer": 0,
-      "total_miner_reward": 0
+      "gas_price": "0",
+      "coinbase_transfer": "0",
+      "total_miner_reward": "0"
     }
   ]
 }
@@ -157,11 +157,11 @@ app.get('/v1/transactions', async (req, res, next) => {
   "blocks": [
     {
       "block_number": 12006597,
-      "miner_reward": 89103402731082940,
+      "miner_reward": "89103402731082940",
       "miner": "0xd224ca0c819e8e97ba0136b3b95ceff503b79f53",
-      "coinbase_transfers": 51418761731082940,
+      "coinbase_transfers": "51418761731082940",
       "gas_used": 374858,
-      "gas_price": 237699082668,
+      "gas_price": "237699082668",
       "transactions": [
         {
           "transaction_hash": "0x3c302a865edd01047e5454a28feb4bb91b5e4d880b53ba2b91aec359ebe031a5",
@@ -170,9 +170,9 @@ app.get('/v1/transactions', async (req, res, next) => {
           "eoa_address": "0xf888ac7A3f709d3DA4fabBB04412c479b94FEC94",
           "to_address": "0x111111125434b319222CdBf8C261674aDB56F3ae",
           "gas_used": 292129,
-          "gas_price": 129000000000,
-          "coinbase_transfer": 0,
-          "total_miner_reward": 37684641000000000
+          "gas_price": "129000000000",
+          "coinbase_transfer": "0",
+          "total_miner_reward": "37684641000000000"
         },
         {
           "transaction_hash": "0xb0686a581fde130f5e0621c6aedb2f7b4c33fbc95f89cda0e01833843a4f6b29",
@@ -181,9 +181,9 @@ app.get('/v1/transactions', async (req, res, next) => {
           "eoa_address": "0xD1c1E70325E89bf7d6440Fe9D10802186B21672d",
           "to_address": "0xa57Bd00134B2850B2a1c55860c9e9ea100fDd6CF",
           "gas_used": 82729,
-          "gas_price": 0,
-          "coinbase_transfer": 51418761731082940,
-          "total_miner_reward": 51418761731082940
+          "gas_price": "0",
+          "coinbase_transfer": "51418761731082940",
+          "total_miner_reward": "51418761731082940"
         }
       ]
     }
